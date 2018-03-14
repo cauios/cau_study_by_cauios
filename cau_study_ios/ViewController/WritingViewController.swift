d105f162411a28c146073e7fe4d7c133c9398f63
hohun94 <wifilibe@gmail.com>
2018-03-14 20:45:50 +0900

    1. upload 버튼 구현(20강): upload 버튼을 누르면 waiting이라는 팝업이 뜨고, 실제 사진과 데이터가 파베 데이터베이스로 올라간다.  - 현재 파이어베이스와 연동이 안되서 실제 확인은 불가능하다. 2. 사진 올릴 때 placeholder image 교체(기존에 이미지가 없어서 곤란) 3. 다음에 개발하는 분은 21. share posts 부터 진행할 것!

--- cau_study_ios/ViewController/WritingViewController.swift
+++ cau_study_ios/ViewController/WritingViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseStorage
+import FirebaseDatabase
 
 class WritingViewController: UIViewController {
 
@@ -27,6 +29,40 @@ class WritingViewController: UIViewController {
     
     
     // Buttons
+    @IBAction func uploadButton_Click(_ sender: Any) {
+        ProgressHUD.show("Waiting...", interaction: false)
+        if let profileImg = self.selectedImage, let imageData = UIImageJPEGRepresentation(profileImg, 0.1) {
+            let photoIdString = NSUUID().uuidString
+            print(photoIdString)
+            let storageRef = Storage.storage().reference(forURL: Config.STORAGE_ROOF_REF).child("posts").child(photoIdString)
+            storageRef.putData(imageData, metadata: nil, completion: { (metadata, error) in
+                if error != nil {
+                    ProgressHUD.showError(error!.localizedDescription)
+                    return
+                }
+                let photoUrl = metadata?.downloadURL()?.absoluteString
+                self.sendDataToDatabase(photoUrl: photoUrl!)
+                
+            })
+        } else {
+            ProgressHUD.showError("Profile Image can't be empty")
+        }
+    }
+    
+    func sendDataToDatabase(photoUrl: String) {
+        let ref = Database.database().reference()
+        let postsReference = ref.child("posts")
+        let newPostId = postsReference.childByAutoId().key
+        let newPostReference = postsReference.child(newPostId)
+        newPostReference.setValue(["photoUrl": photoUrl], withCompletionBlock: {
+            (error, ref) in
+            if error != nil {
+                ProgressHUD.showError(error!.localizedDescription)
+                return
+            }
+            ProgressHUD.showSuccess("Sucess")
+        })
+    }
     
     @IBAction func cancelButtonClick(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
