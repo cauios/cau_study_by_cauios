2df4d2d2861a692f54ab303147481a85f5f023ac
minjeong <mjchokorea@gmail.com>
2018-07-02 20:05:46 +0900

    database에 mypost 등록되게

--- cau_study_ios/ViewController/WritingViewController.swift
+++ cau_study_ios/ViewController/WritingViewController.swift
@@ -84,6 +84,18 @@ class WritingViewController: UIViewController {
                 ProgressHUD.showError(error!.localizedDescription)
                 return // withCompletionBlock closure에서 한 것은, if we failed to push this url on the DB, we'll recieve a non zero error object. If we catch the error, we can simply report that to the user as we did before using ProgressHUD.showError method.
             }
+            
+            //민정
+            let myPostRef = Api.MyPosts.REF_MYPOSTS.child(currentUserId).child(newPostId)
+            myPostRef.setValue(true, withCompletionBlock: {(error,ref) in
+                if error != nil {
+                    ProgressHUD.showError(error!.localizedDescription)
+                    return
+                }
+            
+            })
+            
+            
             ProgressHUD.showSuccess("Sucess")
             
             // [Dahye comment] after successfully push the writing data into the DB, change the imageview into the placeholder image and remove the texts in the textview
