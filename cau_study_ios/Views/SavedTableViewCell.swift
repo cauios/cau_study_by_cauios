f4a1ef56d73f755f9efe0c4f64697db0c999ce9c
hohun94 <livekhh@naver.com>
2018-07-21 12:10:57 +0900

    t

---
+++ cau_study_ios/Views/SavedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SavedTableViewCell.swift
+//  cau_study_ios
+//
+//  Created by 강호현 on 2018. 7. 9..
+//  Copyright © 2018년 신형재. All rights reserved.
+//
+
+import UIKit
+
+class SavedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
