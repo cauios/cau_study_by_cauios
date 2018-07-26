bd0470401e516330ba8219a578ef62e16a0b7b25
Dahye <Dahye@adiminui-macbook-pro.local>
2018-07-26 19:58:10 +0900

    Create the 'HelperService' file

--- cau_study_ios/ViewController/WritingViewController.swift
+++ cau_study_ios/ViewController/WritingViewController.swift
@@ -181,16 +181,6 @@ class WritingViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension WritingViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
