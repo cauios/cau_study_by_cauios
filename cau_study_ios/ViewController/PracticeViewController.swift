666a2c98de960d0493caa1bd548f49e49b34f481
namjimin <cauad25@cauui-imac-5.local>
2018-07-30 22:49:29 +0900

    Commit for pull

---
+++ cau_study_ios/ViewController/PracticeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PracticeViewController.swift
+//  cau_study_ios
+//
+//  Created by Dahye on 2018. 7. 30..
+//  Copyright © 2018년 신형재. All rights reserved.
+//
+
+import UIKit
+
+class PracticeViewController: UIViewController {
+
+    @IBAction func practiceButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
