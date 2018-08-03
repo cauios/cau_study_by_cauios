1263581d162115fe4a1646c363a2541eb844c9db
Dahye <Dahye@adiminui-macbook-pro.local>
2018-08-03 17:02:29 +0900

    add images

--- cau_study_ios/Model/Post.swift
+++ cau_study_ios/Model/Post.swift
@@ -24,7 +24,7 @@ class Post {
     var description: String?
     var saved: Bool?// e.g. type이 optional String으로 설정되어 있음!
     var timestamp: Int?
-    var wanted: Bool? // [0726] Dahye
+    var wanted: Bool?
 }
 
 
