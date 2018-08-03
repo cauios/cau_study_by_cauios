54d8ed8ea1796a47e396408996c30dabb31f7f8c
Dahye <Dahye@adiminui-macbook-pro.local>
2018-08-03 21:10:24 +0900

    implement the UI of searchView

---
+++ cau_study_ios/Views/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  cau_study_ios
+//
+//  Created by Dahye on 2018. 8. 3..
+//  Copyright © 2018년 신형재. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
