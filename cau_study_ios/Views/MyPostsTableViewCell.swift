bdebc13e9d8e4121304e9b2f18432b9c9ec3f153
Dahye <Dahye@adiminui-macbook-pro.local>
2018-08-08 22:18:41 +0900

    update design of myPostCells

--- cau_study_ios/Views/MyPostsTableViewCell.swift
+++ cau_study_ios/Views/MyPostsTableViewCell.swift
@@ -15,6 +15,14 @@ class MyPostsTableViewCell: UITableViewCell {
     @IBOutlet weak var exploreCategoryLabel: UILabel!
     @IBOutlet weak var exploreWanted: UILabel!
     
+    @IBOutlet weak var profCateView: UIView!
+    @IBOutlet weak var profContentView: UIView!
+    @IBOutlet weak var profTimestampLabel: UILabel!
+    
+    @IBOutlet weak var profFinImageView: UIImageView!
+    @IBOutlet weak var profFinLabel: UILabel!
+    
+    
     var post: Post? {
         didSet {
             updateView()
@@ -26,13 +34,101 @@ class MyPostsTableViewCell: UITableViewCell {
         exploreCategoryLabel.text = post?.category
         exploreTagsLabel.text = post?.tags
         
+        // [0808 Dahye]
+        setTimestamp()
+        
+        // [0807 Dahye] Make corners of Views rounded
+        profCateView.layer.cornerRadius = 10.0
+        profContentView.layer.cornerRadius = 10.0
+        profCateView.layer.borderWidth = 0.7
+        profCateView.layer.borderColor = UIColor(red: 237.0/255.0, green: 238.0/255.0, blue: 239.0/255.0, alpha: 1.0).cgColor
+        profContentView.layer.borderWidth = 1.0
+        profContentView.layer.borderColor = UIColor(red: 237.0/255.0, green: 238.0/255.0, blue: 239.0/255.0, alpha: 1.0).cgColor
+        
+        
         if (post?.wanted)! {
             exploreWanted.text = "모집중"
         } else {
             exploreWanted.text = "모집마감"
         }
         
+        // [0731 Dahye] for category image
+        
+        if post?.category == "학업" {
+            if post?.wanted == false {
+                profCateView?.backgroundColor = UIColor(red: 197/255.0, green: 197/255.0, blue: 197/255.0, alpha: 1)
+                profFinImageView?.image = #imageLiteral(resourceName: "finicon")
+                profFinLabel?.text = "마감"
+                
+            } else {
+                profCateView?.backgroundColor = UIColor(red: 202/255.0, green: 237/255.0, blue: 253/255.0, alpha: 1.0)
+                profFinImageView?.image = nil
+                profFinLabel?.text = nil
+            }
+        }
+        if post?.category == "취업" {
+            if post?.wanted == false {
+                profCateView?.backgroundColor = UIColor(red: 197.0/255.0, green: 197/255.0, blue: 197/255.0, alpha: 1.0)
+                profFinImageView?.image = #imageLiteral(resourceName: "finicon")
+                profFinLabel?.text = "마감"
+                
+            } else {
+                profCateView?.backgroundColor = UIColor(red: 255/255.0, green: 219/255.0, blue: 217/255.0, alpha: 1.0)
+                profFinImageView?.image = nil
+                profFinLabel?.text = nil
+            }
+        }
+        if post?.category == "어학" {
+            if post?.wanted == false {
+                profCateView?.backgroundColor = UIColor(red: 197/255.0, green: 197/255.0, blue: 197/255.0, alpha: 1.0)
+                profFinImageView?.image = #imageLiteral(resourceName: "finicon")
+                profFinLabel?.text = "마감"
+            } else {
+                profCateView?.backgroundColor = UIColor(red: 255/255.0, green: 237/255.0, blue: 165/255.0, alpha: 1.0)
+                profFinImageView?.image = nil
+                profFinLabel?.text = nil
+            }
+        }
+        
  
+    }
+    
+    
+    func setTimestamp() {
+        if let timestamp = post?.timestamp {
+            print(timestamp)
+            let timestampDate = Date(timeIntervalSince1970: TimeInterval(timestamp))
+            let now = Date()
+            let components = Set<Calendar.Component>([.second, .minute, .hour, .day, .weekOfMonth])
+            let timeDiff = Calendar.current.dateComponents(components, from: timestampDate, to: now)
+            
+            var postTimestampText = ""
+            
+            // [0726] Dahye: Handle each case
+            
+            if timeDiff.second! <= 0 {
+                postTimestampText = "Now"
+            }
+            if timeDiff.second! > 0 && timeDiff.minute! == 0 {
+                postTimestampText = (timeDiff.second == 1) ? "\(timeDiff.second!) second ago" : "\(timeDiff.second!) seconds ago"
+            }
+            if timeDiff.minute! > 0 && timeDiff.hour! == 0 {
+                postTimestampText = (timeDiff.minute == 1) ? "\(timeDiff.minute!) minute ago" : "\(timeDiff.minute!) minutes ago"
+            }
+            if timeDiff.hour! > 0 && timeDiff.day! == 0 {
+                postTimestampText = (timeDiff.hour == 1) ? "\(timeDiff.hour!) hour ago" : "\(timeDiff.hour!) hours ago"
+            }
+            if timeDiff.day! > 0 && timeDiff.weekOfMonth! == 0 {
+                postTimestampText = (timeDiff.day == 1) ? "\(timeDiff.day!) day ago" : "\(timeDiff.day!) days ago"
+            }
+            if timeDiff.weekOfMonth! > 0 {
+                postTimestampText = (timeDiff.weekOfMonth == 1) ? "\(timeDiff.weekOfMonth!) week ago" : "\(timeDiff.weekOfMonth!) weeks ago"
+            }
+            
+            profTimestampLabel.text = postTimestampText
+            
+            
+        }
     }
 
     override func awakeFromNib() {
