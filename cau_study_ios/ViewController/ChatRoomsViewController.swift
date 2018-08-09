deaa2b7f87890ca1a471ad64f88c53b16dbaddf2
Dahye <Dahye@adiminui-macbook-pro.local>
2018-08-09 19:21:01 +0900

    Update the design of chat rooms view

--- cau_study_ios/ViewController/ChatRoomsViewController.swift
+++ cau_study_ios/ViewController/ChatRoomsViewController.swift
@@ -21,6 +21,7 @@ class ChatRoomsViewController: UIViewController,UITableViewDelegate,UITableViewD
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addNavBarImage() // [0809 Dahye] call func to show navilogo 
         self.uid = Auth.auth().currentUser?.uid
         self.getChatroomsList()
         // Do any additional setup after loading the view.
@@ -41,6 +42,27 @@ class ChatRoomsViewController: UIViewController,UITableViewDelegate,UITableViewD
             self.chattableview.reloadData()
         })
     }
+    
+    // [0809 Dahye] add logo in the middle of the navibar
+    func addNavBarImage() {
+        let navController = navigationController!
+        
+        let image = #imageLiteral(resourceName: "logo")
+        let imageView = UIImageView(image: image)
+        
+        let bannerWidth = navController.navigationBar.frame.size.width / 2
+        let bannerHeight = navController.navigationBar.frame.size.height / 2
+        
+        let bannerX = bannerWidth / 2 - image.size.width / 2
+        let bannerY = bannerHeight / 2 - image.size.height / 2
+        
+        imageView.frame = CGRect(x: bannerX, y: bannerY, width: bannerWidth, height: bannerHeight)
+        imageView.contentMode = .scaleAspectFit
+        
+        navigationItem.titleView = imageView
+        
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.chatrooms.count
